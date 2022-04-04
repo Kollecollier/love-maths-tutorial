6fddbdc74d9317ebd45fc2f55457652091a63202
Kollecollier <kristoffer.collier@live.se>
2022-04-04 13:43:02 +0000

    Set up game structure

---
+++ assets/css/js/script.js
@@ -0,0 +1,37 @@
+function runGame() {
+
+}
+
+function checkAnswer () {
+
+
+}
+
+function calcualteCorrectAnswer () {
+
+
+}
+
+function incrementScore () {
+
+
+}
+
+function increment WrongAnswer() {
+
+
+}
+
+function dispayAdditionalQuestion () {
+ 
+    
+}
+
+function dispaySubtractQuestion() {
+
+
+}
+
+function dispayMultiplyQuestion() {
+    
+}
